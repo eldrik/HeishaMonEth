dd6d88311d3212232d9eb07f23db528184eef972
eldrik <github@eldrik.de>
2023-10-27 08:39:43 +0200

    Update gpio.h
    changed gpios

--- HeishaMon/gpio.h
+++ HeishaMon/gpio.h
@@ -4,7 +4,7 @@
 #define NUMGPIO 3
 
 struct gpioSettingsStruct {
-  unsigned int gpioPin[NUMGPIO] = {1, 3, 16};
+  unsigned int gpioPin[NUMGPIO] = {0, 0, 0}; // von 1 3 16 auf 0 0 0 abgeändert um Pin 1 und 3 (TX / RX) weiter für die Kommunikation zur Wärmepumpe zu benutzen und Pin 16 für Ethernet
   unsigned int gpioMode[NUMGPIO] = {INPUT_PULLUP, INPUT_PULLUP, INPUT_PULLUP};
 };
 
